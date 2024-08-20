a7a01021de4c77e30729a042e4264b4bc1deb245
hipolitomck <hipolitomckillip58@gmail.com>
2024-08-20 14:55:23 +0800

    Delete corp_init.py

--- corp_init.py
+++
@@ -1,7 +0,0 @@
-# corp_init.py
-
-Corpid = "your_corpid"        # 替换为你的企业ID
-Agentid = "your_agentid"      # 替换为你的企业应用ID
-Corpsecret = "your_corpsecret" # 替换为你的企业应用凭证密钥
-Touser = "@all"               # 默认@all发送给所有成员, 你也可以指定具体用户
-Media_id = ""                 # 可选：媒体文件ID，不填则发送文本消息
